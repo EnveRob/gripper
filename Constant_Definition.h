fe2edd5fbdb251cfa7814253c5aedeff124c58c7
a891021960035 <a891021960035@gmail.com>
2023-05-10 17:57:01 +0800

    reindent gripper.ino and Constant_Definition.h

--- Constant_Definition.h
+++ Constant_Definition.h
@@ -6,75 +6,73 @@
 /*======== Hardware Definition ========*/
 /*==== Pin Definition ====*/
 /*== USB Baud Rate ==*/
-#define BAUDRATE_USB					115200
+#define BAUDRATE_USB 115200
 
 /*== LED Control ==*/
-#define PIN_LED_ONBOARD					13
+#define PIN_LED_ONBOARD 13
 
 /*== Motor ==*/
 /*= Encoder =*/
-#define PIN_ENCODER_A					2	// PD2
-#define PIN_ENCODER_B					3	// PD3
+#define PIN_ENCODER_A 2 // PD2
+#define PIN_ENCODER_B 3 // PD3
 
-#define MASK_ENCODER					0x0C //00001100
-#define MASK_ENCODER_PLUS				0x04 //00000100
+#define MASK_ENCODER 0x0C      // 00001100
+#define MASK_ENCODER_PLUS 0x04 // 00000100
 
-#define DATA_ENCODER					PIND
+#define DATA_ENCODER PIND
 
 /*= Driver L298 =*/
-#define PIN_INA							6
-#define PIN_INB							5
-#define PIN_ENABLE					4
+#define PIN_INA 6
+#define PIN_INB 5
+#define PIN_ENABLE 4
 
 /*== Hardware Setup Time ==*/
-#define TIME_SETUP_DELAY_MS				500
+#define TIME_SETUP_DELAY_MS 500
 
 /*======== Software Definition ========*/
 /*==== USB Constant ====*/
-#define TIME_USB_MS						5000
+#define TIME_USB_MS 5000
 
 /*==== USB Plot Constant ====*/
-#define TIME_USB_MS_PLOT				10
+#define TIME_USB_MS_PLOT 10
 
 /*==== LED Constant ====*/
-#define TIME_LED_FLASH_MS				25
+#define TIME_LED_FLASH_MS 25
 
-#define LED_ONBOARD_ON()				digitalWrite(PIN_LED_ONBOARD, HIGH )
-#define LED_ONBOARD_OFF()				digitalWrite(PIN_LED_ONBOARD, LOW )
+#define LED_ONBOARD_ON() digitalWrite(PIN_LED_ONBOARD, HIGH)
+#define LED_ONBOARD_OFF() digitalWrite(PIN_LED_ONBOARD, LOW)
 
 /*==== Motor Constant ====*/
-#define TIME_ENCODER_MS					1
-#define TIME_CONTROL_MS					1
-#define TIME_SPEED_CONTROL_MS   10
-#define TIME_FORCE_CONTROL_MS   10
+#define TIME_ENCODER_MS 1
+#define TIME_CONTROL_MS 1
+#define TIME_SPEED_CONTROL_MS 10
+#define TIME_FORCE_CONTROL_MS 10
 
+#define MODE_STOP 0
+#define MODE_VOLT 1
+#define MODE_REVERSE 2
 
+#define MOTOR_CW(_PWM) analogWrite(PIN_INA, 0), analogWrite(PIN_INB, _PWM), digitalWrite(PIN_ENABLE, HIGH)
+#define MOTOR_CCW(_PWM) analogWrite(PIN_INA, _PWM), analogWrite(PIN_INB, 0), digitalWrite(PIN_ENABLE, HIGH)
+#define MOTOR_STOP() analogWrite(PIN_INA, 0), analogWrite(PIN_INB, 0), digitalWrite(PIN_ENABLE, LOW)
+#define MAX_DUTY 255
 
-#define MODE_STOP				0
-#define MODE_VOLT				1
-#define MODE_REVERSE				2
+#define DEAD_ZONE 5
 
-#define MOTOR_CW(_PWM )					analogWrite(PIN_INA, 0 ), analogWrite(PIN_INB, _PWM ), digitalWrite(PIN_ENABLE, HIGH )
-#define MOTOR_CCW(_PWM )				analogWrite(PIN_INA, _PWM ), analogWrite(PIN_INB, 0 ), digitalWrite(PIN_ENABLE, HIGH )
-#define MOTOR_STOP()					analogWrite(PIN_INA, 0 ), analogWrite(PIN_INB, 0 ), digitalWrite(PIN_ENABLE, LOW )
-#define MAX_DUTY						255
+#define MOTOR_ENCODER_PPR 32000 // 16(gear ratio)*500(encoder ppr)*4
 
-#define DEAD_ZONE						5
+#define FORCE_CONTROL_P 150
+#define FORCE_CONTROL_I 0
+#define FORCE_CONTROL_D 0
 
-#define MOTOR_ENCODER_PPR				32000 // 16(gear ratio)*500(encoder ppr)*4
+#define AMPLITUDE_DEFAULT 16000
+#define PERIOD_DEFAULT_MS 2000.0
 
-#define FORCE_CONTROL_P			  150
-#define FORCE_CONTROL_I  			0
-#define FORCE_CONTROL_D  			0
+#define SET_VOLT 497 // 500g -> 498
+#define MAX_VOLT 573 // 835g
+#define LOAD_CELL_OFFSET 700
 
-#define AMPLITUDE_DEFAULT				16000
-#define PERIOD_DEFAULT_MS				2000.0
-
-#define SET_VOLT              497 //500g -> 498
-#define MAX_VOLT              573 //835g
-#define LOAD_CELL_OFFSET      700
-
-#define X_AXIS                A0
-#define Y_AXIS                A1
+#define X_AXIS A0
+#define Y_AXIS A1
 
 #endif
--- gripper.ino
+++ gripper.ino
@@ -6,8 +6,7 @@
 #include <std_msgs/UInt8.h>
 #include <std_msgs/Bool.h>
 
-
-void gripper_cmd_callback(const std_msgs::UInt8& cmd);
+void gripper_cmd_callback(const std_msgs::UInt8 &cmd);
 ros::NodeHandle nh;
 geometry_msgs::Vector3 force;
 std_msgs::UInt8 cmd;
@@ -17,24 +16,24 @@ ros::Publisher shut_pub("shutdown", &shutdown);
 ros::Subscriber<std_msgs::UInt8> cmd_sub("gripper_cmd", &gripper_cmd_callback);
 
 /*== Timer Variables ==*/
-Constant_Timer Timer_Encoder ;
-Constant_Timer Timer_USB ;
-Constant_Timer Timer_Control ;
-Constant_Timer Timer_Speed ;
+Constant_Timer Timer_Encoder;
+Constant_Timer Timer_USB;
+Constant_Timer Timer_Control;
+Constant_Timer Timer_Speed;
 
 /*== Motor Variables ==*/
-volatile signed long slEncoder_Counter = 0 ;
-volatile signed long slEncoder_Counter_last = 0 ;
+volatile signed long slEncoder_Counter = 0;
+volatile signed long slEncoder_Counter_last = 0;
 
-volatile float slLoadCell_Volt_Read_x = 0 ;
-volatile float slLoadCell_Volt_Read_y = 0 ;
+volatile float slLoadCell_Volt_Read_x = 0;
+volatile float slLoadCell_Volt_Read_y = 0;
 volatile float slOutput_test = 0;
 
-//static int iVolt = 0 ;
-static int iVolt = SET_VOLT ;
-static int iReverse = 10 ;
-static char cControlMode = MODE_STOP ;
-static bool bReverse = true ;
+// static int iVolt = 0 ;
+static int iVolt = SET_VOLT;
+static int iReverse = 10;
+static char cControlMode = MODE_STOP;
+static bool bReverse = true;
 
 /*== Moving average filter setup ==*/
 int ma_counter = 0;
@@ -45,18 +44,20 @@ volatile float avg_x;
 volatile float avg_y;
 int i;
 
-void setup(){
+void setup()
+{
   nh.initNode();
   nh.advertise(force_pub);
   nh.advertise(shut_pub);
   nh.subscribe(cmd_sub);
-	Hardware_Setup() ;
-	Software_Setup() ;
+  Hardware_Setup();
+  Software_Setup();
 }
 
-void loop(){
-	Control_Task() ; // 進行Motor Control
-  Encoder_Task() ; // 讀取Encoder
+void loop()
+{
+  Control_Task(); // 進行Motor Control
+  Encoder_Task(); // 讀取Encoder
   force_pub.publish(&force);
   shut_pub.publish(&shutdown);
   nh.spinOnce();
@@ -65,86 +66,93 @@ void loop(){
 
 /*========= Function =========*/
 /*==== Setup Function ====*/
-void Hardware_Setup(void ){
-	/*== LED Setup Function ==*/
-		// config IO direction of LED
-	pinMode(PIN_LED_ONBOARD, OUTPUT ) ;
+void Hardware_Setup(void)
+{
+  /*== LED Setup Function ==*/
+  // config IO direction of LED
+  pinMode(PIN_LED_ONBOARD, OUTPUT);
 
-	/*== Motor Setup Function ==*/
-	pinMode(PIN_INA, OUTPUT ) ;
-	pinMode(PIN_INB, OUTPUT ) ;
-	pinMode(PIN_ENABLE, OUTPUT ) ;
+  /*== Motor Setup Function ==*/
+  pinMode(PIN_INA, OUTPUT);
+  pinMode(PIN_INB, OUTPUT);
+  pinMode(PIN_ENABLE, OUTPUT);
 
-	pinMode(PIN_ENCODER_A, INPUT ) ;
-	pinMode(PIN_ENCODER_B, INPUT ) ;
+  pinMode(PIN_ENCODER_A, INPUT);
+  pinMode(PIN_ENCODER_B, INPUT);
   pinMode(X_AXIS, INPUT);
   pinMode(Y_AXIS, INPUT);
 
-	attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_A ), Encoder_Task, CHANGE ) ;
-	attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_B ), Encoder_Task, CHANGE ) ;
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_A), Encoder_Task, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_B), Encoder_Task, CHANGE);
 }
 
-void Software_Setup(void ){
-	/*== Timer Setup Function ==*/
-	Timer_USB.Timer_Task(0 ) ;
-	Timer_Encoder.Timer_Task(0 ) ;
-	Timer_Control.Timer_Task(0 ) ;
-  Timer_Speed.Timer_Task(0 ) ;
+void Software_Setup(void)
+{
+  /*== Timer Setup Function ==*/
+  Timer_USB.Timer_Task(0);
+  Timer_Encoder.Timer_Task(0);
+  Timer_Control.Timer_Task(0);
+  Timer_Speed.Timer_Task(0);
 
-	/*== Motor Control Setup Function ==*/
-	slEncoder_Counter = 0 ;
-	MOTOR_STOP() ;
+  /*== Motor Control Setup Function ==*/
+  slEncoder_Counter = 0;
+  MOTOR_STOP();
 
-	/*== Setup Finished!! ==*/
+  /*== Setup Finished!! ==*/
 }
 
 /*== Motor Function ==*/
-void Encoder_Task(void ){
-	/*== Encoder Conunter ==*/
-	static uint8_t _pin_data = 0x00 ;
-	static uint8_t _pin_data_last = 0x00 ;
-	static uint8_t _pin_temp_1 = 0x00 ;
-	static uint8_t _pin_temp_2 = 0x00 ;
+void Encoder_Task(void)
+{
+  /*== Encoder Conunter ==*/
+  static uint8_t _pin_data = 0x00;
+  static uint8_t _pin_data_last = 0x00;
+  static uint8_t _pin_temp_1 = 0x00;
+  static uint8_t _pin_temp_2 = 0x00;
 
-	_pin_data = DATA_ENCODER ;
-	_pin_temp_1 = _pin_data^_pin_data_last ;
-	_pin_temp_2 = (_pin_data >> 1 )^_pin_data_last ;
+  _pin_data = DATA_ENCODER;
+  _pin_temp_1 = _pin_data ^ _pin_data_last;
+  _pin_temp_2 = (_pin_data >> 1) ^ _pin_data_last;
 
-	if(_pin_temp_1 & MASK_ENCODER ){
-		if(_pin_temp_2 & MASK_ENCODER_PLUS ) slEncoder_Counter++ ;
-		else slEncoder_Counter-- ;
-	}
+  if (_pin_temp_1 & MASK_ENCODER)
+  {
+    if (_pin_temp_2 & MASK_ENCODER_PLUS)
+      slEncoder_Counter++;
+    else
+      slEncoder_Counter--;
+  }
 
-	_pin_data_last = _pin_data ;
+  _pin_data_last = _pin_data;
 }
 
-void Control_Task(void ){
+void Control_Task(void)
+{
   static int _n = 0;
-	static int _iAbs_duty ;
-  static int _iCurrentTime = 0; //ms
-  static int _iPrevTime = 0; //ms
-	static signed long _slOutput_temp ;
-  static signed long _slVolt_err ;
-	static signed long _slDelta_err_v ;
-	static signed long _slIntegral_err_v ;
-	static signed long _slPrev_err_v ;
+  static int _iAbs_duty;
+  static int _iCurrentTime = 0; // ms
+  static int _iPrevTime = 0;    // ms
+  static signed long _slOutput_temp;
+  static signed long _slVolt_err;
+  static signed long _slDelta_err_v;
+  static signed long _slIntegral_err_v;
+  static signed long _slPrev_err_v;
   static signed long _slEncoder_Counter_temp;
   static bool _bSign_output = true;
-	static float _fTemp1, _fTemp2 = 0.0 ;
-  
+  static float _fTemp1, _fTemp2 = 0.0;
+
   // Read load cell (LC) value, and add an offset 700 (the direction of LC in assembly is opposite)
   slLoadCell_Volt_Read_x = LOAD_CELL_OFFSET - analogRead(X_AXIS);
-  slLoadCell_Volt_Read_y = LOAD_CELL_OFFSET - analogRead(Y_AXIS); 
+  slLoadCell_Volt_Read_y = LOAD_CELL_OFFSET - analogRead(Y_AXIS);
   // Moving average (ma) filter to smooth out the LC value
-  avg_x=0;
-  avg_y=0;
-  ma_counter = ma_counter%avg_num;
+  avg_x = 0;
+  avg_y = 0;
+  ma_counter = ma_counter % avg_num;
   avg_volt_x[ma_counter] = slLoadCell_Volt_Read_x;
   avg_volt_y[ma_counter] = slLoadCell_Volt_Read_y;
-  for(i = 0; i<avg_num; i++)
+  for (i = 0; i < avg_num; i++)
   {
-    avg_x += avg_volt_x[i]/avg_num;
-    avg_y += avg_volt_y[i]/avg_num;
+    avg_x += avg_volt_x[i] / avg_num;
+    avg_y += avg_volt_y[i] / avg_num;
   }
   ma_counter++;
   // ma end. output is "avg_x" and "avg_y"
@@ -153,77 +161,93 @@ void Control_Task(void ){
   force.y = avg_y;
   force.z = 0;
 
-  if(cControlMode == MODE_STOP){
+  if (cControlMode == MODE_STOP)
+  {
     _slOutput_temp = 0;
-    if(_slOutput_temp > 0 ) MOTOR_CW(abs(_slOutput_temp )) ;
-    else MOTOR_CCW(abs(_slOutput_temp )) ;
+    if (_slOutput_temp > 0)
+      MOTOR_CW(abs(_slOutput_temp));
+    else
+      MOTOR_CCW(abs(_slOutput_temp));
   }
-	
-  else if(cControlMode == MODE_VOLT){
-    if(Timer_Control.Timer_Task(TIME_FORCE_CONTROL_MS ) ){
+
+  else if (cControlMode == MODE_VOLT)
+  {
+    if (Timer_Control.Timer_Task(TIME_FORCE_CONTROL_MS))
+    {
       /*==安全機制==*/
       /*=超過MAX_VOLT觸發MODE_REVERSE=*/
-      if(avg_y >= MAX_VOLT || avg_x >= MAX_VOLT){
+      if (avg_y >= MAX_VOLT || avg_x >= MAX_VOLT)
+      {
         cControlMode = MODE_REVERSE;
         shutdown.data = true;
       }
-	    
+
       /*==PID Voltage Control==*/
       /*=達到SET_VOLT(iVolt)夾爪停止轉動=*/
-      _slVolt_err = (signed long)iVolt - avg_y ;
-      _slDelta_err_v = _slVolt_err - _slPrev_err_v ;
-      _slIntegral_err_v += _slVolt_err ;
+      _slVolt_err = (signed long)iVolt - avg_y;
+      _slDelta_err_v = _slVolt_err - _slPrev_err_v;
+      _slIntegral_err_v += _slVolt_err;
 
-      _slOutput_temp = ((_slVolt_err * FORCE_CONTROL_P + 
-               _slIntegral_err_v * FORCE_CONTROL_I + 
-                _slDelta_err_v * FORCE_CONTROL_D ) >> 9 ) ;
-      _slPrev_err_v = _slVolt_err ;
-      slOutput_test = _slOutput_temp ;
+      _slOutput_temp = ((_slVolt_err * FORCE_CONTROL_P +
+                         _slIntegral_err_v * FORCE_CONTROL_I +
+                         _slDelta_err_v * FORCE_CONTROL_D) >>
+                        9);
+      _slPrev_err_v = _slVolt_err;
+      slOutput_test = _slOutput_temp;
 
-      if(_slOutput_temp >= 0 ) _bSign_output = true ;
-      else _bSign_output = false ;
+      if (_slOutput_temp >= 0)
+        _bSign_output = true;
+      else
+        _bSign_output = false;
 
-      _slOutput_temp = abs(_slOutput_temp) ;
+      _slOutput_temp = abs(_slOutput_temp);
 
-      if(_slOutput_temp > MAX_DUTY ){
-        _slOutput_temp = MAX_DUTY ;
+      if (_slOutput_temp > MAX_DUTY)
+      {
+        _slOutput_temp = MAX_DUTY;
       }
 
-      if(_bSign_output ){
-        MOTOR_CW(_slOutput_temp ) ;
+      if (_bSign_output)
+      {
+        MOTOR_CW(_slOutput_temp);
       }
-      else{
-        MOTOR_CCW(_slOutput_temp ) ;
+      else
+      {
+        MOTOR_CCW(_slOutput_temp);
       }
     }
   }
-  else if(cControlMode == MODE_REVERSE){
-    if(Timer_Speed.Timer_Task(TIME_SPEED_CONTROL_MS ) ){
-    _slOutput_temp = iReverse;
-    MOTOR_CCW(_slOutput_temp) ;
+  else if (cControlMode == MODE_REVERSE)
+  {
+    if (Timer_Speed.Timer_Task(TIME_SPEED_CONTROL_MS))
+    {
+      _slOutput_temp = iReverse;
+      MOTOR_CCW(_slOutput_temp);
     }
-    if(bReverse){
+    if (bReverse)
+    {
       bReverse = false;
       _slEncoder_Counter_temp = slEncoder_Counter;
     }
-    if(abs(slEncoder_Counter - _slEncoder_Counter_temp) > 25) 
+    if (abs(slEncoder_Counter - _slEncoder_Counter_temp) > 25)
       cControlMode = MODE_STOP;
   }
 }
 
-void gripper_cmd_callback(const std_msgs::UInt8& cmd)
+void gripper_cmd_callback(const std_msgs::UInt8 &cmd)
 {
-  switch(cmd.data){
-    case MODE_STOP:
-      cControlMode = MODE_STOP;
-      break;
-    case MODE_VOLT:
-      cControlMode = MODE_VOLT;
-      shutdown.data = false;
-      break;
-    case MODE_REVERSE:
-      cControlMode = MODE_REVERSE;
-      bReverse = true;
-      break;
+  switch (cmd.data)
+  {
+  case MODE_STOP:
+    cControlMode = MODE_STOP;
+    break;
+  case MODE_VOLT:
+    cControlMode = MODE_VOLT;
+    shutdown.data = false;
+    break;
+  case MODE_REVERSE:
+    cControlMode = MODE_REVERSE;
+    bReverse = true;
+    break;
   }
 }
